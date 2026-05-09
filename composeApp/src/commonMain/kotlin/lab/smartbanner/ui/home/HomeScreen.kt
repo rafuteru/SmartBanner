0a6d122e49b9d27a2f9af683a6e66110578dfef4
aj <developer.amitjaiswal@gmail.com>
2026-05-09 12:52:14 +0530

    Polish MVP experience

--- composeApp/src/commonMain/kotlin/lab/smartbanner/ui/home/HomeScreen.kt
+++ composeApp/src/commonMain/kotlin/lab/smartbanner/ui/home/HomeScreen.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
