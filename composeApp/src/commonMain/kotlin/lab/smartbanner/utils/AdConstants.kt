d5aa3a55d6922c60da5fd5574cbf52834b4c8dcd
aj <developer.amitjaiswal@gmail.com>
2026-05-18 16:16:36 +0530

    Make use of ad test IDs

--- composeApp/src/commonMain/kotlin/lab/smartbanner/utils/AdConstants.kt
+++ composeApp/src/commonMain/kotlin/lab/smartbanner/utils/AdConstants.kt
@@ -3,20 +3,28 @@ package lab.smartbanner.utils
 import lab.smartbanner.getPlatform
 
 object AdConstants {
-    // Check if we are running on Android
     private val isAndroid = getPlatform().name.startsWith("Android")
 
+    // Set to true during development to ensure ads always load
+    private const val USE_TEST_ADS = true
+
+    // Google's official Test Banner IDs
+    private const val ANDROID_TEST_ID = "ca-app-pub-3940256099942544/6300978111"
+    private const val IOS_TEST_ID = "ca-app-pub-3940256099942544/2934735716"
+
     // --- HOME GRID BANNER ---
-    private const val ANDROID_HOME_ID = "ca-app-pub-6016705200049000/2969709036" // Replace with real Android ID
-    private const val IOS_HOME_ID = "ca-app-pub-6016705200049000/5412039547"     // Replace with real iOS ID
+    private const val ANDROID_HOME_ID = "ca-app-pub-6016705200049000/2969709036"
+    private const val IOS_HOME_ID = "ca-app-pub-6016705200049000/5412039547"
 
     val HOME_GRID_BANNER_ID: String
-        get() = if (isAndroid) ANDROID_HOME_ID else IOS_HOME_ID
+        get() = if (USE_TEST_ADS) (if (isAndroid) ANDROID_TEST_ID else IOS_TEST_ID)
+        else (if (isAndroid) ANDROID_HOME_ID else IOS_HOME_ID)
 
     // --- PREVIEW BOTTOM BANNER ---
-    private const val ANDROID_PREVIEW_ID = "ca-app-pub-6016705200049000/5404300683" // Replace with real Android ID
-    private const val IOS_PREVIEW_ID = "ca-app-pub-6016705200049000/9642928599"     // Replace with real iOS ID
+    private const val ANDROID_PREVIEW_ID = "ca-app-pub-6016705200049000/5404300683"
+    private const val IOS_PREVIEW_ID = "ca-app-pub-6016705200049000/9642928599"
 
     val PREVIEW_BOTTOM_BANNER_ID: String
-        get() = if (isAndroid) ANDROID_PREVIEW_ID else IOS_PREVIEW_ID
+        get() = if (USE_TEST_ADS) (if (isAndroid) ANDROID_TEST_ID else IOS_TEST_ID)
+        else (if (isAndroid) ANDROID_PREVIEW_ID else IOS_PREVIEW_ID)
 }
