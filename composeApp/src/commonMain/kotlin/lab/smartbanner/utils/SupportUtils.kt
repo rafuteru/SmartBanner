0977b4c6adaebe79f2d0dff293fa4ef3f63c9dd2
aj <developer.amitjaiswal@gmail.com>
2026-05-19 05:57:23 +0530

    Support email updated

--- composeApp/src/commonMain/kotlin/lab/smartbanner/utils/SupportUtils.kt
+++ composeApp/src/commonMain/kotlin/lab/smartbanner/utils/SupportUtils.kt
@@ -3,7 +3,7 @@ package lab.smartbanner.utils
 import lab.smartbanner.getPlatform
 
 object SupportConstants {
-    const val SUPPORT_EMAIL = "coffeeat202labs@gmail.com"
+    const val SUPPORT_EMAIL = "support@shuvian.com"
 }
 
 fun contactSupport(subject: String, body: String) {
